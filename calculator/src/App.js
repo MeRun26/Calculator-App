cbf280f44c6172d08a77e017b72192452ef02c8f
Miran <Emilbekov.m091@mail.ru>
2023-11-18 19:14:34 +0600

    completing2 a calculator task

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -21,57 +21,9 @@ const buttons = [
 ];
 
 const Calculator = () => {
-	// const [operand1, setOperand1] = useState("");
-	// const [operator, setOperator] = useState("");
-	// const [operand2, setOperand2] = useState("");
 	const [displayValue, setDisplayValue] = useState("");
-	// const [isResult, setIsResult] = useState(false);
+
 	const [resultColor, setResultColor] = useState(styles.defaultColor);
-
-	// // asfsfsdfgdgdf
-	// const handleNumberClick = (num) => {
-	// 	if (isResult) {
-	// 		setOperand1(num);
-	// 		setIsResult(false);
-	// 	} else if (!operator) {
-	// 		setOperand1(operand1 + num);
-	// 	} else {
-	// 		setOperand2(operand2 + num);
-	// 	}
-	// };
-
-	// const handleOperatorClick = (op) => {
-	// 	if (op === "C") {
-	// 		setOperand1("");
-	// 		setOperand2("");
-	// 		setOperator("");
-	// 		setIsResult(false);
-	// 	} else if (op === "=") {
-	// 		if (operand2 && operator) {
-	// 			let result;
-	// 			switch (operator) {
-	// 				case "+":
-	// 					result = parseInt(operand1) + parseInt(operand2);
-	// 					break;
-	// 				case "-":
-	// 					result = parseInt(operand1) - parseInt(operand2);
-	// 					break;
-	// 				// Add cases for other operators if needed
-	// 				default:
-	// 					break;
-	// 			}
-	// 			setOperand1(result.toString());
-	// 			setOperator("");
-	// 			setOperand2("");
-	// 			setIsResult(true);
-	// 		}
-	// 	} else {
-	// 		setOperator(op);
-	// 		setIsResult(false);
-	// 	}
-	// };
-
-	// // asfsfsdfgdgdf
 
 	const handleButtonClick = (value) => {
 		if (value === "=") {
